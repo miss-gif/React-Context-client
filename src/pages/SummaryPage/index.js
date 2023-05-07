c503bbc6d8004252ed09a2961fb324848bea7a0f
miss <ddc.akari@gmail.com>
2023-05-08 08:23:09 +0900

    주문 확인 페이지 구현

--- src/pages/SummaryPage/index.js
+++ src/pages/SummaryPage/index.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { OrderContext } from "./../../context/OrderContext";
 
-const SummaryPage = () => {
+const SummaryPage = ({ setStep }) => {
   const [checked, setChecked] = useState(false);
   const [orderDetails] = useContext(OrderContext);
 
@@ -12,12 +12,34 @@ const SummaryPage = () => {
     </li>
   ));
 
+  const hasOptions = orderDetails.totals.products > 0;
+  let optionsDisplay = null;
+
+  if (hasOptions) {
+    const optionsArray = Array.from(orderDetails.options.keys());
+    const optionList = optionsArray.map((key) => <li key={key}>{key}</li>);
+    optionsDisplay = (
+      <>
+        <h2>옵션: {orderDetails.totals.options}</h2>
+        <ul>{optionList}</ul>
+      </>
+    );
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStep(2);
+  };
+
   return (
     <div>
       <h1>주문 확인</h1>
       <h2>여행 상품: {orderDetails.totals.products}</h2>
       <ul>{productList}</ul>
-      <form>
+
+      {optionsDisplay}
+
+      <form onSubmit={handleSubmit}>
         <input
           type="checkbox"
           checked={checked}
