0c5b987f21194ca4bc521255e8c40b97028585e8
miss <ddc.akari@gmail.com>
2023-05-08 20:38:49 +0900

    중간저장

--- src/pages/CompletePage/index.js
+++ src/pages/CompletePage/index.js
@@ -1,9 +1,11 @@
-import React, { useContext, useEffect } from "react";
-import { OrderContext } from "./../../context/OrderContext";
+import React, { useContext, useEffect, useState } from "react";
+import { OrderContext } from "../../context/OrderContext";
 import axios from "axios";
 
-const CompletePage = () => {
+const CompletePage = ({ setStop }) => {
+  const [orderHistory, setorderHistory] = useState([]);
   const [orderData] = useContext(OrderContext);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     orderCompleted(orderData);
@@ -12,16 +14,45 @@ const CompletePage = () => {
   const orderCompleted = async (orderData) => {
     try {
       const response = await axios.post(
-        "http://localhost:4000/oredr",
+        "http://localhost:4000/order",
         orderData
       );
       console.log("response", response);
+      setorderHistory(response.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <div>CompletePage</div>;
+  const orderTable = orderHistory.map((item) => (
+    <tr key={item.orderNumber}>
+      <td>{item.orderNumber}</td>
+      <td>{item.price}</td>
+    </tr>
+  ));
+
+  if (loading) {
+    return <div>...loading</div>;
+  } else {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h2>주문이 성공했습니다.</h2>
+        <h3>지금까지 모든 주문</h3>
+        <table style={{ margin: "auto" }}>
+          <tbody>
+            <tr>
+              <th>number</th>
+              <th>price</th>
+            </tr>
+            {orderTable}
+          </tbody>
+        </table>
+        <br />
+        <button onClick={() => setStep(0)}>첫 페이지로</button>
+      </div>
+    );
+  }
 };
 
 export default CompletePage;
