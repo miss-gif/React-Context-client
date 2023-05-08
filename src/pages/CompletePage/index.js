0246ec85fdb5bd6162b541e34ee1aac8b5263618
miss <ddc.akari@gmail.com>
2023-05-08 20:22:16 +0900

    중간 저장

--- src/pages/CompletePage/index.js
+++ src/pages/CompletePage/index.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { OrderContext } from "./../../context/OrderContext";
+import axios from "axios";
 
 const CompletePage = () => {
+  const [orderData] = useContext(OrderContext);
+
+  useEffect(() => {
+    orderCompleted(orderData);
+  }, [orderData]);
+
+  const orderCompleted = async (orderData) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/oredr",
+        orderData
+      );
+      console.log("response", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>CompletePage</div>;
 };
 
